5ceff5f98689504f19636f1c609723cdb41c1598
Greg Anders <ganders@utexas.edu>
2017-07-12 14:38:39 -0500

    Tweak setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-mkdir -p $HOME/.config
-
-cp -r $(pwd)/nvim $HOME/.config/nvim
 cp -r $(pwd)/vim $HOME/.vim
 cp $(pwd)/vimrc $HOME/.vimrc
-ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
 cp $(pwd)/tmux.conf $HOME/.tmux.conf
 
-if hash git 2>/dev/null && hash zsh 2>/dev/null; then
+if hash nvim 2>/dev/null; then
+    mkdir -p $HOME/.config
+    cp -r $(pwd)/nvim $HOME/.config/nvim
+    ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
+fi
+
+if hash git 2>/dev/null; then
     git clone --recursive https://github.com/gpanders/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md; do
-        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/!(README.md); do
+        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile##*/}"
     done
-    chsh -s /bin/zsh
+    if hash zsh 2>/dev/null; then
+        sudo chsh -s $(which zsh) ${SUDO_USER:-$USER}
+    fi
+else
+    echo "Git not found; not cloning prezto"
 fi
