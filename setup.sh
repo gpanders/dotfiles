39bbe75857ffc4fd0ec05197a9ab5849eb231ad0
Greg Anders <greg.p.anders@gmail.com>
2018-10-30 12:30:14 -0600

    Add prezto as a submodule and delete old symlinks when running setup.sh

--- setup.sh
+++ setup.sh
@@ -4,52 +4,66 @@ shopt -s extglob
 
 curr_dir=$(pwd)
 
-if [ -s $HOME/.vim ]; then
+if [ -h $HOME/.vim ]; then
+  rm $HOME/.vim
+elif [ -d $HOME/.vim ]; then
   mv $HOME/.vim $HOME/.vim.bak
 fi
-ln -s $curr_dir/vim $HOME/.vim
+ln -sv $curr_dir/vim $HOME/.vim
 
-if [ -s $HOME/.tmux ]; then
+if [ -h $HOME/.tmux ]; then
+  rm $HOME/.tmux
+elif [ -d $HOME/.tmux ]; then
   mv $HOME/.tmux $HOME/.tmux.bak
 fi
-ln -s $curr_dir/tmux $HOME/.tmux
+ln -sv $curr_dir/tmux $HOME/.tmux
 
-if [ -s $HOME/.vimrc ]; then
+if [ -h $HOME/.vimrc ]; then
+  rm $HOME/.vimrc
+elif [ -f $HOME/.vimrc ]; then
   mv $HOME/.vimrc $HOME/.vimrc.bak
 fi
-ln -s $curr_dir/vimrc $HOME/.vimrc
+ln -sv $curr_dir/vimrc $HOME/.vimrc
 
-if [ -s $HOME/.tmux.conf ]; then
+if [ -h $HOME/.tmux.conf ]; then
+  rm $HOME/.tmux.conf
+elif [ -f $HOME/.tmux.conf ]; then
   mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 fi
-ln -s $curr_dir/tmux.conf $HOME/.tmux.conf
+ln -sv $curr_dir/tmux.conf $HOME/.tmux.conf
 
 mkdir -p $HOME/.config/nvim
-if [ -s $HOME/.config/nvim/init.vim ]; then
+if [ -h $HOME/.config/nvim/init.vim ]; then
+  rm $HOME/.config/nvim/init.vim
+elif [ -f $HOME/.config/nvim/init.vim ]; then
   mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.bak
 fi
-ln -s $curr_dir/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sv $curr_dir/nvim/init.vim $HOME/.config/nvim/init.vim
 
 if [[ "$SHELL" =~ "bash" ]]; then
-  if [ -s $HOME/.fzf.bash ]; then
+  if [ -h $HOME/.fzf.bash ]; then
+    rm $HOME/.fzf.bash
+  elif [ -f $HOME/.fzf.bash ]; then
     mv $HOME/.fzf.bash $HOME/.fzf.bash.bak
   fi
-  ln -s $curr_dir/fzf.bash $HOME/.fzf.bash
+  ln -sv $curr_dir/fzf.bash $HOME/.fzf.bash
 
-  if [ -s $HOME/.bashrc ]; then
+  if [ -h $HOME/.bashrc ]; then
+    rm $HOME/.bashrc
+  elif [ -f $HOME/.bashrc ]; then
     mv $HOME/.bashrc $HOME/.bashrc.bak
   fi
-  ln -s $curr_dir/bashrc $HOME/.bashrc
+  ln -sv $curr_dir/bashrc $HOME/.bashrc
 fi
 
 # Upgrade vim-plug and install vim plugins
-vim -u $curr_dir/vim/plugins.vim -c PlugUpgrade -c PlugInstall -c q -c q
-
-# Initialize git submodules
-git submodule update --init --remote
+if [ ! -d $curr_dir/vim/plug ]; then
+  vim -u $curr_dir/vim/plugins.vim -c PlugUpgrade -c PlugInstall -c qall!
+fi
 
 if hash tmux 2>/dev/null; then
   # Install tmux plugins in a background session
+  git submodule update --init --remote tmux/plugins/tpm
   tmux new-session -s install_plugins -d "tmux run-shell $HOME/.tmux/plugins/tpm/bindings/install_plugins"
 fi
 
@@ -66,27 +80,18 @@ if ! git config --global --get user.email 1>/dev/null ; then
 fi
 
 if hash zsh 2>/dev/null; then
-  install_prezto=0
   read -r -p "Install prezto? [y/N] " ans
   if [[ "$ans" =~ ^([Yy]|[Yy][Ee][Ss])+$ ]]; then
-    install_prezto=1
-  fi
-
-  if [ $install_prezto -eq 1 ]; then
-    if hash git 2>/dev/null; then
-      if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
-        git clone --quiet --recursive https://github.com/gpanders/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-        cd "${ZDOTDIR:-$HOME}"/.zprezto && git submodule update --init --recursive && cd -
-      else
-        echo ".zprezto dir already exists, not cloning git repo"
-      fi
-      for zfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/!(README.md); do
-        if [ ! -s "${ZDOTDIR:-$HOME}/.${zfile##*/}" ]; then
-            ln -vs "$zfile" "${ZDOTDIR:-$HOME}/.${zfile##*/}"
+    if [ ! -s $HOME/.zprezto ]; then
+      git submodule update --init --recursive zprezto
+      ln -s $curr_dir/zprezto $HOME/.zprezto 
+      for zfile in $curr_dir/zprezto/runcoms/!(README.md); do
+        if [ ! -s "$HOME/.${zfile##*/}" ]; then
+            ln -vs "$zfile" "$HOME/.${zfile##*/}"
         fi
       done
     else
-      echo "Git not found; not cloning prezto"
+      echo ".zprezto dir already exists"
     fi
   fi
 
