942fcbe5f7d767c25dd61e6f100a1df2bef07b70
Gregory Anders <greg@gpanders.com>
2018-11-14 21:17:39 -0700

    Remove proxy settings

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -9,20 +9,24 @@
       (width . 200)
       (height . 60))))
  '(display-line-numbers t)
+ '(enable-recursive-minibuffers t)
  '(evil-magit-state (quote normal))
  '(evil-magit-use-y-for-yank nil)
+ '(evil-want-C-u-scroll t)
+ '(evil-want-keybinding nil)
  '(inhibit-startup-screen t)
  '(initial-frame-alist
    (quote
     ((vertical-scroll-bars)
      (width . 200)
      (height . 60))))
+ '(ivy-use-virtual-buffers t)
  '(markdown-command "pandoc")
  '(markdown-open-command "/usr/local/bin/macdown")
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (flx delight diminish solarized-theme dracula-theme exec-path-from-shell evil-magit markdown-mode smart-mode-line better-defaults leuven-theme evil-collection counsel ivy projectile evil-surround evil-commentary evil-leader key-chord f magit evil use-package)))
+    (use-package-ensure-system-package flx delight diminish solarized-theme dracula-theme exec-path-from-shell evil-magit markdown-mode smart-mode-line better-defaults leuven-theme evil-collection counsel ivy projectile evil-surround evil-commentary evil-leader key-chord f magit evil use-package)))
  '(scroll-bar-mode nil)
  '(sml/no-confirm-load-theme t)
  '(sml/theme (quote light))
