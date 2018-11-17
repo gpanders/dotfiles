b3055574267ff88a8fec3c9e6ea155a14ca59097
Greg Anders <greg@gpanders.com>
2018-11-17 13:17:25 -0700

    On second thought, do use a custom.el file

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default-frame-alist (quote ((width . 160) (height . 50))))
+ '(global-display-line-numbers-mode t)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(initial-frame-alist (quote ((width . 160) (height . 50))))
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:weight normal :height 120 :width normal :family "Fira Code")))))
