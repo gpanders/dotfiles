696a652bdaa03b1e0046df376e84b7807a0bc1ed
Greg Anders <greg@gpanders.com>
2018-11-17 20:41:33 -0700

    Update custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,11 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-verbose nil)
  '(default-frame-alist (quote ((width . 160) (height . 50))))
+ '(global-auto-revert-mode t)
+ '(global-auto-revert-non-file-buffers t)
  '(global-display-line-numbers-mode t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(initial-frame-alist (quote ((width . 160) (height . 50))))
+ '(package-selected-packages
+   (quote
+    (cmake-ide use-package-ensure-system-package solarized-theme smex rtags projectile multi-term markdown-mode load-theme-buffer-local leuven-theme key-chord general flx f exec-path-from-shell evil-surround evil-magit evil-leader evil-commentary evil-collection dracula-theme delight counsel company color-theme-buffer-local better-defaults)))
+ '(ring-bell-function (quote ignore))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
