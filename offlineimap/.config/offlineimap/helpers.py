048c98519e44c518fe3ee743feaa76ff490631cf
Greg Anders <greg.p.anders@gmail.com>
2018-12-03 06:43:14 -0700

    Add GMail account to OffineIMAP

--- offlineimap/.config/offlineimap/helpers.py
+++ offlineimap/.config/offlineimap/helpers.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from subprocess import check_output
 
+def get_secret(account, linum):
+    return check_output("pass " + account, shell=True).splitlines()[linum]
+
 def get_pass(account):
-    return check_output("pass " + account, shell=True).splitlines()[0]
+    return get_secret(account, 0)
--- offlineimap/.config/offlineimap/notmuch.sh
+++ offlineimap/.config/offlineimap/notmuch.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+  echo "$0: missing required argument"
+  return 1
+fi
+
 notmuch new
 notmuch tag +inbox +unread -new -- tag:new
-notmuch tag +archive -inbox -unread -- folder:Personal/Archive
-notmuch tag +sent -inbox -unread -- folder:Personal/Sent
-notmuch tag +trash -inbox -unread -archive -- folder:Personal/Trash
+notmuch tag +archive -inbox -unread -- folder:$1/Archive
+notmuch tag +sent -inbox -unread -- folder:$1/Sent OR folder:$1/Sent\ Mail
+notmuch tag +trash -inbox -unread -archive -- folder:$1/Trash
