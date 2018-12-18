b2ecab91612e5a74f442e65a6b131025de87a433
Greg Anders <greg@gpanders.com>
2018-12-18 12:38:03 -0700

    Use minimal zsh configuration instead of prezto

---
+++ zsh/.fzf.zsh
@@ -0,0 +1,40 @@
+FZF_DEFAULT_OPTS=""
+
+# Color
+# -----
+typeset -A fzf_colors
+fzf_colors["Solarized Dark"]="dark,fg:-1,bg:-1,hl:#268bd2,fg+:#93a1a1,bg+:#073642,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
+fzf_colors["Solarized Light"]="light,fg:-1,bg:-1,hl:#268bd2,fg+:#586e75,bg+:#eee8d5,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
+fzf_colors["One Dark"]="dark,fg:-1,bg:-1,hl:#98c379,fg+:#fefefe,bg+:#3e4452,hl+:#98c379,info:#61afef,prompt:#61afef,pointer:#e5c07b,marker:#e5c07b,spinner:#61afef"
+
+FZF_COLOR_SCHEME="One Dark"
+
+FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color ${fzf_colors["$FZF_COLOR_SCHEME"]}"
+
+# Use ripgrep or ag if available
+if (( $+commands[rg] )); then
+  export FZF_CTRL_T_COMMAND="rg --files"
+  _fzf_compgen_path() {
+    rg --files "$1"
+  }
+elif (( $+commands[ag] )); then
+  export FZF_CTRL_T_COMMAND="ag -g ''"
+  _fzf_compgen_path() {
+    ag -g '' "$1"
+  }
+fi
+
+if (( $+commands[tree] )); then
+  export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2>/dev/null | head -200'"
+  export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+fi
+
+if [ -n "$TMUX_PANE" ]; then
+  export FZF_TMUX=1
+  alias fzf="fzf-tmux -d40%"
+else
+  export FZF_TMUX=0
+  FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height 40% --inline-info --reverse"
+fi
+
+export FZF_DEFAULT_OPTS
