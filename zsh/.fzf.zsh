432fc676b4562334c97accd28a628193f8364334
Greg Anders <greg@gpanders.com>
2018-12-18 18:06:15 -0700

    Don't clobber bindkeys from plugins

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -7,7 +7,7 @@ fzf_colors["Solarized Dark"]="dark,fg:-1,bg:-1,hl:#268bd2,fg+:#93a1a1,bg+:#07364
 fzf_colors["Solarized Light"]="light,fg:-1,bg:-1,hl:#268bd2,fg+:#586e75,bg+:#eee8d5,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
 fzf_colors["One Dark"]="dark,fg:-1,bg:-1,hl:#98c379,fg+:#fefefe,bg+:#3e4452,hl+:#98c379,info:#61afef,prompt:#61afef,pointer:#e5c07b,marker:#e5c07b,spinner:#61afef"
 
-FZF_COLOR_SCHEME="One Dark"
+export FZF_COLOR_SCHEME="One Dark"
 
 FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color ${fzf_colors["$FZF_COLOR_SCHEME"]}"
 
