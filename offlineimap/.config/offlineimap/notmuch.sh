9f3eda1284be466e306d8a178a5113d9e8fde2c7
Greg Anders <greg@gpanders.com>
2018-12-29 14:35:35 -0600

    [offlineimap] Update notmuch.sh script

--- offlineimap/.config/offlineimap/notmuch.sh
+++ offlineimap/.config/offlineimap/notmuch.sh
@@ -6,7 +6,8 @@ if [ "$#" -eq 0 ]; then
 fi
 
 notmuch new
-notmuch tag +inbox +unread -new -- tag:new
+notmuch tag +unread -new -- tag:new
+notmuch tag +inbox -- folder:$1/INBOX
 notmuch tag +archive -inbox -unread -- folder:$1/Archive
 notmuch tag +sent -inbox -unread -- folder:$1/Sent OR folder:$1/Sent\ Mail
 notmuch tag +trash -inbox -unread -archive -- folder:$1/Trash
