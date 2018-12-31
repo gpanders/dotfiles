f64905f090cb0f3220b0e8e0a847eced85ce1e9e
Greg Anders <greg@gpanders.com>
2018-12-31 11:04:46 -0600

    Add evil mode plugins and leader mappings

--- emacs/.emacs.d/lisp/init-evil.el
+++ emacs/.emacs.d/lisp/init-evil.el
@@ -1,9 +1,25 @@
 (use-package evil
   :ensure t
   :init
-  (setq evil-default-state 'emacs)
   (setq evil-want-C-u-scroll t)
   :config
-  (evil-mode 1))
+  (use-package evil-surround
+    :ensure t
+    :config
+    (evil-surround-mode t))
+  (use-package evil-commentary
+    :ensure t
+    :delight
+    :config
+    (evil-commentary-mode t))
+
+  (defvar evil-leader-map (make-sparse-keymap)
+    "Keymap for \"leader key\" shortcuts.")
+
+  (define-key evil-normal-state-map "," evil-leader-map)
+  (define-key evil-leader-map "b" 'switch-to-buffer)
+  (define-key evil-leader-map "e" 'find-file)
+  (define-key evil-leader-map "w" 'save-buffer)
+  (define-key evil-normal-state-map (kbd "SPC SPC") 'counsel-M-x))
 
 (provide 'init-evil)
