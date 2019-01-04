142e36495bd733660c70f33afc46e0d563d09b6e
Gregory Anders <greg@gpanders.com>
2019-01-04 14:29:38 -0600

    [offlineimap] Update notmuch script

--- offlineimap/.config/offlineimap/notmuch.sh
+++ offlineimap/.config/offlineimap/notmuch.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-if [ "$#" -eq 0 ]; then
-  echo "$0: missing required argument"
-  return 1
-fi
-
 notmuch new
-notmuch tag +unread -new -- tag:new
-notmuch tag +inbox -- folder:$1/INBOX
-notmuch tag +archive -inbox -unread -- folder:$1/Archive
-notmuch tag +sent -inbox -unread -- folder:$1/Sent OR folder:$1/Sent\ Mail
-notmuch tag +trash -inbox -unread -archive -- folder:$1/Trash
+notmuch tag +inbox -- folder:/INBOX/
+notmuch tag +archive -inbox -- folder:/Archive/
+notmuch tag +sent -inbox -- folder:/Sent/
+notmuch tag +trash -inbox -archive -- folder:/Trash/
+
+# Remove new tag from anything not in the inbox
+notmuch tag -new -- NOT tag:inbox
