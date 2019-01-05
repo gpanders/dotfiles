d0d70c0e9514e27158a93495554f79cef8732ded
Gregory Anders <greg@gpanders.com>
2019-01-04 18:02:48 -0600

    [emacs] Add rust-mode package

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -6,10 +6,10 @@
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (auctex flx solarized-theme flycheck sane-term evil-commentary evil-leader evil-surround yasnippet counsel ivy delight magit exec-path-from-shell projectile smex company-lsp lsp-ui cquery atom-one-dark-theme evil use-package))))
+    (rust-mode auctex flx solarized-theme flycheck sane-term evil-commentary evil-leader evil-surround yasnippet counsel ivy delight magit exec-path-from-shell projectile smex company-lsp lsp-ui cquery atom-one-dark-theme evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:weight normal :height 120 :family "Fira Mono")))))
+ '(default ((t (:weight medium :height 120 :width medium :family "Fira Mono")))))
