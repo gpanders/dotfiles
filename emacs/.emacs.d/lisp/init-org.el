1f2e3acc1262cf575dcdf6c9396287abcbb93db3
Greg Anders <greg@gpanders.com>
2019-01-09 09:42:44 -0700

    [emacs] Update gtd file names

--- emacs/.emacs.d/lisp/init-org.el
+++ emacs/.emacs.d/lisp/init-org.el
@@ -1,12 +1,11 @@
-(defcustom org-gtd-directory nil
-  "Location of GTD org mode files."
-  :type 'string)
+(defvar org-gtd-directory nil "Location of GTD org mode files.")
 
 (setq org-gtd-directory
       (cond ((eq system-type 'darwin) "~/Documents/Notes/gtd/")
 	    ((eq system-type 'gnu/linux) "~/notes/gtd/")))
+
 (setq org-agenda-files `(,(expand-file-name "inbox.org" org-gtd-directory)
-			 ,(expand-file-name "gtd.org" org-gtd-directory)
+			 ,(expand-file-name "actions.org" org-gtd-directory)
 			 ,(expand-file-name "tickler.org" org-gtd-directory)))
 (setq org-capture-templates `(("t" "Todo [inbox]" entry
 			       (file+headline ,(expand-file-name "inbox.org" org-gtd-directory) "Tasks")
@@ -14,7 +13,7 @@
 			      ("T" "Tickler" entry
 			       (file+headline ,(expand-file-name "tickler.org" org-gtd-directory) "Tickler")
 			       "* %i%? \n %U")))
-(setq org-refile-targets `((,(expand-file-name "gtd.org" org-gtd-directory) :maxlevel . 3)
+(setq org-refile-targets `((,(expand-file-name "actions.org" org-gtd-directory) :maxlevel . 3)
 			   (,(expand-file-name "someday.org" org-gtd-directory) :level . 1)
 			   (,(expand-file-name "tickler.org" org-gtd-directory) :maxlevel . 2)))
 (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
