b6a789695aa6fc99d155414ffeb3573add4bc678
Greg Anders <greg@gpanders.com>
2019-01-14 09:37:44 -0700

    [emacs] Variety of updates
    * Add smartparens package
    * Crib company configuration from Spacemacs
    * Disable sane-term package, bind C-x t to simply 'eshell
    * Spiff up evil config
    ** Use <leader><leader> as M-x
    ** Make some modes default to Emacs state (for now, Info and Custom)
    ** Unbind RET and SPC in normal and motion states. I don't use them
       anyway and they just conflict with more useful bindings
    ** SPC u in normal state binds to universal-argument
    * Convert tabs to spaces in init-org.el
    * Add 'ec' alias to zsh for "emacsclient -nc"

--- emacs/.emacs.d/lisp/init-org.el
+++ emacs/.emacs.d/lisp/init-org.el
@@ -2,20 +2,25 @@
 
 (setq org-gtd-directory
       (cond ((eq system-type 'darwin) "~/Documents/Notes/gtd/")
-	    ((eq system-type 'gnu/linux) "~/notes/gtd/")))
+        ((eq system-type 'gnu/linux) "~/notes/gtd/")))
 
 (setq org-agenda-files `(,(expand-file-name "inbox.org" org-gtd-directory)
-			 ,(expand-file-name "actions.org" org-gtd-directory)
-			 ,(expand-file-name "tickler.org" org-gtd-directory)))
+             ,(expand-file-name "projects.org" org-gtd-directory)
+             ,(expand-file-name "actions.org" org-gtd-directory)
+             ,(expand-file-name "tickler.org" org-gtd-directory)))
 (setq org-capture-templates `(("i" "Inbox" entry
-			       (file+headline ,(expand-file-name "inbox.org" org-gtd-directory) "Inbox")
-			       "* TODO %i%?")
-			      ("T" "Tickler" entry
-			       (file+headline ,(expand-file-name "tickler.org" org-gtd-directory) "Tickler")
-			       "* %i%? \n %U")))
-(setq org-refile-targets `((,(expand-file-name "actions.org" org-gtd-directory) :maxlevel . 3)
-			   (,(expand-file-name "someday.org" org-gtd-directory) :level . 1)
-			   (,(expand-file-name "tickler.org" org-gtd-directory) :maxlevel . 2)))
+                   (file+headline ,(expand-file-name "inbox.org" org-gtd-directory) "Inbox")
+                   "* TODO %i%?")
+                  ("T" "Tickler" entry
+                   (file+headline ,(expand-file-name "tickler.org" org-gtd-directory) "Tickler")
+                   "* %i%?\n %U")
+                  ("d" "Diary" entry
+                   (file ,(expand-file-name (concat "diary/" (format-time-string "%Y-%m-%d") ".org") (file-name-directory (directory-file-name org-gtd-directory))))
+                   "* %i%?")))
+(setq org-refile-targets `((,(expand-file-name "projects.org" org-gtd-directory) :level . 1)
+               (,(expand-file-name "actions.org" org-gtd-directory) :maxlevel . 1)
+               (,(expand-file-name "someday.org" org-gtd-directory) :level . 1)
+               (,(expand-file-name "tickler.org" org-gtd-directory) :maxlevel . 2)))
 (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
 
 (setq org-hide-leading-stars t)
