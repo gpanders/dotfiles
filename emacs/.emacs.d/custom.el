35e4b09a799a64f0db710764e8684b9cda675830
Gregory Anders <greg@gpanders.com>
2019-02-08 15:21:41 -0700

    [emacs] Disable "reverting buffer" messages

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-verbose nil)
  '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(evil-search-module (quote evil-search))
