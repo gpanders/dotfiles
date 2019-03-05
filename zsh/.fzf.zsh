93fe2ca281ba69953b575b06db680f30c1c3a9fb
Greg Anders <greg@gpanders.com>
2019-03-05 13:32:46 -0700

    [zsh] Add base16-eighties colorscheme to fzf

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -6,8 +6,9 @@ typeset -A fzf_colors
 fzf_colors["Solarized Dark"]="dark,fg:-1,bg:-1,hl:#268bd2,fg+:#93a1a1,bg+:#073642,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
 fzf_colors["Solarized Light"]="light,fg:-1,bg:-1,hl:#268bd2,fg+:#586e75,bg+:#eee8d5,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
 fzf_colors["One Dark"]="dark,fg:-1,bg:-1,hl:#98c379,fg+:#fefefe,bg+:#3e4452,hl+:#98c379,info:#61afef,prompt:#61afef,pointer:#e5c07b,marker:#e5c07b,spinner:#61afef"
+fzf_colors["Base16 Eighties"]="bg+:#393939,bg:#2d2d2d,spinner:#66cccc,hl:#6699cc,fg:#a09f93,header:#6699cc,info:#ffcc66,pointer:#66cccc,marker:#66cccc,fg+:#e8e6df,prompt:#ffcc66,hl+:#6699cc"
 
-export FZF_COLOR_SCHEME="One Dark"
+export FZF_COLOR_SCHEME="Base16 Eighties"
 
 FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color ${fzf_colors["$FZF_COLOR_SCHEME"]}"
 
