4556cad6a52922e5c5a3d80a61a53aaf26d08199
Greg Anders <greg@gpanders.com>
2019-03-05 16:28:25 -0700

    [offlineimap] Ensure messages in trash only have trash tag

--- offlineimap/.config/offlineimap/notmuch.sh
+++ offlineimap/.config/offlineimap/notmuch.sh
@@ -4,7 +4,7 @@ notmuch new
 notmuch tag +inbox -- folder:/INBOX/
 notmuch tag +archive -- folder:/Archive/
 notmuch tag +sent -- folder:/Sent/
-notmuch tag +trash -archive -- folder:/Trash/
+notmuch tag --remove-all +trash -- folder:/Trash/
 
 # Remove new tag from anything not in the inbox
 notmuch tag -new -inbox -- NOT tag:inbox
