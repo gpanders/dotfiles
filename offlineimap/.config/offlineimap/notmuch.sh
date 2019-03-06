64685bdb0f7c5751ecaf7a328e452f8e509a10d5
Greg Anders <greg@gpanders.com>
2019-03-05 17:52:31 -0700

    [offlineimap] Put mail directly underneath mail folder

--- offlineimap/.config/offlineimap/notmuch.sh
+++ offlineimap/.config/offlineimap/notmuch.sh
@@ -4,7 +4,7 @@ notmuch new
 notmuch tag +inbox -- folder:/INBOX/
 notmuch tag +archive -- folder:/Archive/
 notmuch tag +sent -- folder:/Sent/
-notmuch tag --remove-all +trash -- folder:/Trash/
+notmuch tag +trash -archive -- folder:/Trash/
 
 # Remove new tag from anything not in the inbox
-notmuch tag -new -inbox -- NOT tag:inbox
+notmuch tag -unread -inbox -- NOT folder:/INBOX/
