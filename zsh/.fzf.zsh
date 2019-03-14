885174d3dacde7705891f7dc7934650f50697c87
Greg Anders <greg@gpanders.com>
2019-03-13 20:54:34 -0600

    Update fzf configuration

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,41 +1,55 @@
 FZF_DEFAULT_OPTS=""
 
 # Color
-# -----
 typeset -A fzf_colors
 fzf_colors["Solarized Dark"]="dark,fg:-1,bg:-1,hl:#268bd2,fg+:#93a1a1,bg+:#073642,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
 fzf_colors["Solarized Light"]="light,fg:-1,bg:-1,hl:#268bd2,fg+:#586e75,bg+:#eee8d5,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
 fzf_colors["One Dark"]="dark,fg:-1,bg:-1,hl:#98c379,fg+:#fefefe,bg+:#3e4452,hl+:#98c379,info:#61afef,prompt:#61afef,pointer:#e5c07b,marker:#e5c07b,spinner:#61afef"
-fzf_colors["Base16 Eighties"]="bg+:#393939,bg:#2d2d2d,spinner:#66cccc,hl:#6699cc,fg:#a09f93,header:#6699cc,info:#ffcc66,pointer:#66cccc,marker:#66cccc,fg+:#e8e6df,prompt:#ffcc66,hl+:#6699cc"
+fzf_colors["Eighties"]="bg+:#393939,bg:#2d2d2d,spinner:#66cccc,hl:#6699cc,fg:#a09f93,header:#6699cc,info:#ffcc66,pointer:#66cccc,marker:#66cccc,fg+:#e8e6df,prompt:#ffcc66,hl+:#6699cc"
 
-export FZF_COLOR_SCHEME="Base16 Eighties"
+export FZF_COLOR_SCHEME="Eighties"
 
 FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color ${fzf_colors["$FZF_COLOR_SCHEME"]}"
 
-# Use ripgrep or ag if available
-if (( $+commands[rg] )); then
-  export FZF_CTRL_T_COMMAND="rg --files"
-  _fzf_compgen_path() {
-    rg --files "$1"
-  }
+# Set default command
+if (( $+commands[fd] )); then
+    export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git"
+    export FZF_ALT_C_COMMAND="fd -t d"
+    _fzf_compgen_path() {
+        fd --hidden --follow --exclude ".git" . "$1"
+    }
+    _fzf_compgen_dir() {
+        fd --type d --hidden --follow --exclude ".git" . "$1"
+    }
+elif (( $+commands[rg] )); then
+    export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
+    _fzf_compgen_path() {
+        rg --files --hidden --glob '!.git' "$1"
+    }
 elif (( $+commands[ag] )); then
-  export FZF_CTRL_T_COMMAND="ag -g ''"
-  _fzf_compgen_path() {
-    ag -g '' "$1"
-  }
+    export FZF_DEFAULT_COMMAND="ag -g ''"
+    _fzf_compgen_path() {
+        ag -g '' "$1"
+    }
+fi
+
+if [ ! -z "$FZF_DEFAULT_COMMAND" ]; then
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
 
 if (( $+commands[tree] )); then
-  export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2>/dev/null | head -200'"
-  export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+    export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2>/dev/null | head -200'"
+    export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 fi
 
 if [ -n "$TMUX_PANE" ]; then
-  export FZF_TMUX=1
-  alias fzf="fzf-tmux -d40%"
+    export FZF_TMUX=1
+    alias fzf="fzf-tmux -d40%"
 else
-  export FZF_TMUX=0
-  FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height 40% --inline-info --reverse"
+    export FZF_TMUX=0
+    FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height 40%"
 fi
+
+FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --inline-info --reverse"
 
 export FZF_DEFAULT_OPTS
