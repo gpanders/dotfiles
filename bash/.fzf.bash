1662b7a30ebf4bef6e18fef9ab823d8ee432b629
Gregory Anders <greg@gpanders.com>
2019-04-09 10:44:08 -0600

    Fix indent in bash scripts

--- bash/.fzf.bash
+++ bash/.fzf.bash
@@ -6,7 +6,7 @@ fi
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-  export PATH="$PATH:$HOME/.fzf/bin"
+    export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Auto-completion
@@ -32,26 +32,26 @@ export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color ${fzf_colors['One Dark']}"
 
 # Use ag
 if hash rg 2>/dev/null; then
-  export FZF_CTRL_T_COMMAND="rg --files"
-  _fzf_compgen_path() {
-    rg --files "$1"
-  }
+    export FZF_CTRL_T_COMMAND="rg --files"
+    _fzf_compgen_path() {
+        rg --files "$1"
+    }
 elif hash ag 2>/dev/null; then
-  export FZF_CTRL_T_COMMAND="ag -g ''"
-  _fzf_compgen_path() {
-    ag -g '' "$1"
-  }
+    export FZF_CTRL_T_COMMAND="ag -g ''"
+    _fzf_compgen_path() {
+        ag -g '' "$1"
+    }
 fi
 
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2>/dev/null | head -200'"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 
 if [ -n "$TMUX_PANE" ]; then
-  export FZF_TMUX=1
-  alias fzf="fzf-tmux -d40%"
+    export FZF_TMUX=1
+    alias fzf="fzf-tmux -d40%"
 else
-  export FZF_TMUX=0
-  export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height 40% --inline-info --reverse"
+    export FZF_TMUX=0
+    export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --height 40% --inline-info --reverse"
 fi
 
 
