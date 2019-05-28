311c472056e5910e1ddc75ccdf49fc11eea9e394
Gregory Anders <greg@gpanders.com>
2019-05-28 13:38:19 -0600

    Cleanup emacs config
    Don't use evil-collection and only use evil mode in programming buffers

--- emacs/.emacs.d/lisp/init-org.el
+++ emacs/.emacs.d/lisp/init-org.el
@@ -1,3 +1,33 @@
+(defvar org-gtd-directory nil "Location of GTD org mode files.")
+
+(setq org-gtd-directory
+      (cond ((eq system-type 'darwin) "~/Documents/ownCloud/GTD/")
+            ((eq system-type 'gnu/linux) "~/notes/gtd/")))
+(defun open-gtd-inbox ()
+  "Open GTD inbox."
+  (interactive)
+  (find-file (expand-file-name "inbox.org" org-gtd-directory)))
+
+(defun open-gtd-actions ()
+  "Open GTD next actions list."
+  (interactive)
+  (find-file (expand-file-name "actions.org" org-gtd-directory)))
+
+(defun open-gtd-projects ()
+  "Open GTD projects list."
+  (interactive)
+  (find-file (expand-file-name "projects.org" org-gtd-directory)))
+
+(defun open-gtd-tickler ()
+  "Open GTD tickler."
+  (interactive)
+  (find-file (expand-file-name "tickler.org" org-gtd-directory)))
+
+(defun open-gtd-someday-maybe ()
+  "Open GTD someday/maybe list."
+  (interactive)
+  (find-file (expand-file-name "someday.org" org-gtd-directory)))
+
 (setq org-agenda-files `(,(expand-file-name "inbox.org" org-gtd-directory)
                          ,(expand-file-name "projects.org" org-gtd-directory)
                          ,(expand-file-name "actions.org" org-gtd-directory)
@@ -21,9 +51,10 @@
 (setq org-hide-leading-stars t)
 (setq org-hide-emphasis-markers t)
 (setq org-html-head
-"<style type=\"text/css\">
+      "<style type=\"text/css\">
   html { font-family: sans-serif; }
   body { padding: 2% 5%; margin: auto; }
+  p, ul, ol, dl { line-height: 1.5em; }
   @media screen and (min-width: 48em) {
     body { max-width: 60%; }
   }
