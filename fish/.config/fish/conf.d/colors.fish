a26b64976dc8424d2930ad56748370d9e2410f8e
Greg Anders <greg@gpanders.com>
2019-08-28 08:15:28 -0600

    Update fish colors, and only set in login shells

--- fish/.config/fish/conf.d/colors.fish
+++ fish/.config/fish/conf.d/colors.fish
@@ -1,9 +1,17 @@
-set -U fish_color_command green
-set -U fish_color_comment yellow
-set -U fish_color_error red
-set -U fish_color_param normal
-set -U fish_color_quote yellow
-set -U fish_color_redirection normal
-set -U fish_color_autosuggestion brblack
-set -U fish_color_operator blue
-set -U fish_pager_color_description yellow
+if status is-login
+    set -U fish_color_command green
+    set -U fish_color_comment yellow
+    set -U fish_color_error red --bold
+    set -U fish_color_param normal
+    set -U fish_color_quote yellow
+    set -U fish_color_redirection normal
+    set -U fish_color_autosuggestion brblack
+    set -U fish_color_operator blue
+    set -U fish_color_end normal
+    set -U fish_color_search_match normal
+    set -U fish_color_cancel black --background=white
+    set -U fish_color_selection normal --background=bryellow
+    set -U fish_pager_color_prefix normal --bold
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description yellow
+end
