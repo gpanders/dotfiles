0dd515be0073d2de032e27ba2149f1e71fb945e4
Greg Anders <greg@gpanders.com>
2019-08-28 20:10:02 -0600

    Fish shell changes
    - Put fisher bootstrapping into a conf.d script
    - Remove pyenv and rbenv conf.d scripts
    - Add !! and !$ substitution

--- fish/.config/fish/conf.d/colors.fish
+++ fish/.config/fish/conf.d/colors.fish
@@ -1,4 +1,4 @@
-if status is-login
+if not set -q fish_user_colors
     set -U fish_color_command green
     set -U fish_color_comment yellow
     set -U fish_color_error red --bold
@@ -8,10 +8,12 @@ if status is-login
     set -U fish_color_autosuggestion brblack
     set -U fish_color_operator blue
     set -U fish_color_end normal
-    set -U fish_color_search_match normal
+    set -U fish_color_search_match normal --background=bryellow
     set -U fish_color_cancel black --background=white
     set -U fish_color_selection normal --background=bryellow
     set -U fish_pager_color_prefix normal --bold
     set -U fish_pager_color_completion normal
     set -U fish_pager_color_description yellow
+
+    set -U fish_user_colors 1
 end
---
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,6 @@
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind -M insert ! bind_bang
+    bind '$' bind_dollar
+    bind -M insert '$' bind_dollar
+end
