7ca5b5b0f0c2a08fe331f560b749e683baad1b41
Greg Anders <greg@gpanders.com>
2019-08-28 19:09:31 -0600

    Fix path in fish tmux function

--- fish/.config/fish/functions/tmux.fish
+++ fish/.config/fish/functions/tmux.fish
@@ -1,4 +1,3 @@
-# Defined in - @ line 1
-function tmux --description 'alias tmux=tmux -f /Users/greande/.config/tmux/tmux.conf'
-	command tmux -f /Users/greande/.config/tmux/tmux.conf $argv;
+function tmux
+	command tmux -f $HOME/.config/tmux/tmux.conf $argv;
 end
