8d2245448cb15585fed35e470780229dbf7c9fcf
Gregory Anders <greg@gpanders.com>
2019-09-17 16:53:29 -0600

    Update ual function to work with BSD date command

--- fish/.config/fish/functions/ual.fish
+++ fish/.config/fish/functions/ual.fish
@@ -18,31 +18,36 @@ function ual
             end
             mkdir -p "$NOTES"
             set NOTE "$NOTES"/$argv[2].md
-            if not test -f "$NOTE"
-                echo "# NAME\n\n$argv[2]\n" > "$NOTE"
+            if not test -f $NOTE
+                echo "# NAME\n\n$argv[2]\n" > $NOTE
             end
-            $EDITOR "$NOTE"
+            $EDITOR $NOTE
         case rm
             if test (count $argv) -lt 2
                 echo "Which ual page do you want to remove?"
                 return
             end
             set NOTE "$NOTES"/$argv[2].md
-            if not test -f "$NOTE"
+            if not test -f $NOTE
                 echo "No ual entry for $argv[2]" >&2
                 return 1
             end
-            rm "$NOTE"
+            rm $NOTE
         case ls
             printf "%s\n" (command ls $NOTES | sed 's/\.[^.]*$//')
         case sync
             echo -n "Syncing notes... "
-            cd "$NOTES"; and git pull >/dev/null 2>&1; and git add .; and git commit -n >/dev/null 2>&1; and git commit -qv 2>/dev/null; and git push >/dev/null 2>&1
+            cd "$NOTES"
+            and git pull >/dev/null 2>&1
+            and git add .
+            and git commit -n >/dev/null 2>&1
+            and git commit -qv 2>/dev/null
+            and git push >/dev/null 2>&1
             echo "Done."
         case '*'
             set NOTE "$NOTES"/$argv[1].md
 
-            if not test -f "$NOTE"
+            if not test -f $NOTE
                 echo "No ual entry for $argv[1]" >&2
                 return 1
             end
@@ -50,7 +55,7 @@ function ual
             set TITLE (echo $argv[1] | tr '[:lower:]' '[:upper:]')
             set SECTION ual
             set AUTHOR "Greg Anders"
-            set DATE (date +'%B %d, %Y' -r "$NOTE")
+            set DATE (date -r $NOTE "+%B %d, %Y")
 
             pandoc \
                 -s \
@@ -59,6 +64,6 @@ function ual
                 -M author="$AUTHOR" \
                 -M section="$SECTION" \
                 -M date="$DATE" \
-                "$NOTE" | groff -T utf8 -man | less -is -+F -+X
+                $NOTE | groff -T utf8 -man | less -is -+F -+X
     end
 end
