b317801f74661678016d38f8af9de596ff4f017b
Gregory Anders <greg@gpanders.com>
2019-09-20 13:23:27 -0600

    Upgrade fish 'venv' function
    venv can now create and remove virtual environments too.

--- fish/.config/fish/completions/venv.fish
+++ fish/.config/fish/completions/venv.fish
@@ -3,5 +3,8 @@ function __fish_complete_venvs
     printf "%s\n" (command ls $VENV_DIR)
 end
 
+complete -x -c venv -n '__fish_use_subcommand' -a 'create' -d 'Create a virtual environment'
+complete -x -c venv -n '__fish_use_subcommand' -a 'rm' -d 'Delete a virtual environment'
+complete -x -c venv -n '__fish_use_subcommand' -a 'ls' -d 'List all virtual environments'
 complete -x -c venv -n '__fish_use_subcommand' -a '(__fish_complete_venvs)'
-complete -x -c venv
+complete -x -c venv -n '__fish_seen_subcommand_from rm' -a '(__fish_complete_venvs)'
--- fish/.config/fish/functions/venv.fish
+++ fish/.config/fish/functions/venv.fish
@@ -3,15 +3,43 @@ function venv
     or set -U VENV_DIR $XDG_DATA_HOME/venv
 
     if test (count $argv) -lt 1
-        echo "Usage: venv <venv>" >&2
+        echo "Usage:
+    venv create <venv>
+    venv rm <venv>
+    venv ls
+    venv <venv>" >&2
         return 1
     end
 
-    if not test -d $VENV_DIR/$argv[1]
-        echo "venv '$argv[1]' not found" >&2
-        return 1
+    switch $argv[1]
+        case create
+            python3 -m venv $VENV_DIR/$argv[2]
+            if test $status
+                echo "Virtual environment $argv[2] created. Activate it using 'venv $argv[2]'"
+            else
+                echo "Something went wrong; virtual environment creation failed" >&2
+                return 1
+            end
+        case ls
+            printf "%s\n" (command ls $VENV_DIR)
+        case rm
+            if not test -d $VENV_DIR/$argv[2]
+                echo "venv '$argv[2]' not found" >&2
+                return 1
+            end
+            rm -rf $VENV_DIR/$argv[2]
+            if test $status
+                echo "Virtual environment '$argv[2]' was successfully deleted"
+            else
+                echo "Something went wrong; virtual environment deletion failed" >&2
+                return 1
+            end
+        case '*'
+            if not test -d $VENV_DIR/$argv[1]
+                echo "venv '$argv[1]' not found" >&2
+                return 1
+            end
+            source $VENV_DIR/$argv[1]/bin/activate.fish
     end
-
-    source $VENV_DIR/$argv[1]/bin/activate.fish
 end
 
