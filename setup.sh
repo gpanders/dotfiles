9e01c4aca4a1abeca2b0874e15eab9bcb40a77f4
Greg Anders <greg@gpanders.com>
2019-09-25 07:41:57 -0600

    Make setup script slightly more portable

--- setup.sh
+++ setup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-shopt -s extglob
-
 install() {
-    if [[ "$OSTYPE" == darwin* ]]; then
-        brew install "$@"
-    elif [[ "$OSTYPE" == linux-gnu ]]; then
-        if hash apt-get 2>/dev/null; then
-            sudo apt-get install -y "$@"
-        elif hash yum 2>/dev/null; then
-            sudo yum install "$@"
-        elif hash pacman 2>/dev/null; then
-            sudo pacman -S "$@"
-        fi
-    fi
+    uname="$(uname)"
+    case $uname in
+        Darwin)
+            brew install "$@"
+            ;;
+        Linux)
+            if hash apt-get 2>/dev/null; then
+                sudo apt-get install -y "$@"
+            elif hash yum 2>/dev/null; then
+                sudo yum install "$@"
+            elif hash pacman 2>/dev/null; then
+                sudo pacman -S "$@"
+            fi
+            ;;
+        *)
+            echo "Unknown OS" >&2
+            return 1
+            ;;
+    esac
 }
 
 if ! hash stow 2>/dev/null; then
@@ -87,10 +93,10 @@ if [ $# -eq 0 ]; then
 
                 # Symlink `mutt` to `neomutt`
                 if [ ! -s "$(dirname "$(command -v neomutt)")/mutt" ]; then
-                    pushd "$(dirname "$(command -v neomutt)")"
+                    pushd "$(dirname "$(command -v neomutt)")" || exit 1
                     sudo ln -s neomutt mutt
                     sudo chown --reference=neomutt mutt
-                    popd
+                    popd || exit 1
                 fi
                 ARGS="$ARGS mutt"
             fi
@@ -140,68 +146,72 @@ for mod in $ARGS; do
     fi
 
     # Handle special cases
-    if [[ "$mod" == "tmux" ]]; then
-        if hash tmux 2>/dev/null; then
-            # Install tmux plugins in a background session
-            git submodule update --init
-            tmux new-session -s install_plugins -d "tmux run-shell $HOME/.tmux/plugins/tpm/bindings/install_plugins"
-        fi
-
-        if ! infocmp tmux-256color &>/dev/null; then
-            tic -x terminfo/tmux-256color.txt
-        fi
-    elif [[ "$mod" == "git" ]]; then
-        # Configure git
-        if [ ! -f "$HOME/.gitconfig" ] && [ ! -f "${XDG_CONFIG_HOME:-$HOME/.config}/git/config" ]; then
-            touch "${XDG_CONFIG_DIR:-$HOME/.config}/git/config"
-        fi
-
-        if ! git config --global --get user.name 1>/dev/null ; then
-            git config --global user.name "Greg Anders"
-        fi
-
-        if ! git config --global --get user.email 1>/dev/null ; then
-            read -r -p "Git email address [greg@gpanders.com]: " ans
-            git config --global user.email "${ans:-greg@gpanders.com}"
-        fi
-
-        # This repository should always use my personal email
-        git config user.email greg@gpanders.com
-
-        git config --global commit.verbose true
-        git config --global rebase.autoSquash true
-        git config --global alias.a add
-        git config --global alias.b branch
-        git config --global alias.cm commit
-        git config --global alias.co checkout
-        git config --global alias.d diff
-        git config --global alias.f fetch
-        git config --global alias.l log
-        git config --global alias.m merge
-        git config --global alias.p push
-        git config --global alias.re reset
-        git config --global alias.st status
-        git config --global alias.snapshot "!git stash && git stash apply -q"
-        git config --global alias.t tag
-
-        # Alias for just the dotfiles repo
-        git config alias.update "!git fetch origin master:master && git rebase --autostash master"
-
-        # Set sendmail settings for git
-        git config --global sendemail.smtpEncryption tls
-        git config --global sendemail.smtpServer mail.gandi.net
-        git config --global sendemail.smtpUser greg@gpanders.com
-        git config --global sendemail.smtpServerPort 587
-    elif [[ "$mod" == "zsh" ]]; then
-        if ! hash antibody 2>/dev/null; then
-            if [[ "$OSTYPE" == darwin* ]]; then
-                brew install getantibody/tap/antibody
-            else
-                mkdir -p "$HOME"/.local
-                curl -sfL git.io/antibody | sh -s - -b "$HOME"/.local/bin
+    case $mod in
+        tmux)
+            if hash tmux 2>/dev/null; then
+                # Install tmux plugins in a background session
+                git submodule update --init
+                tmux new-session -s install_plugins -d "tmux run-shell $HOME/.tmux/plugins/tpm/bindings/install_plugins"
             fi
-        fi
-    fi
+
+            if ! infocmp tmux-256color &>/dev/null; then
+                tic -x terminfo/tmux-256color.txt
+            fi
+            ;;
+        git)
+            # Configure git
+            if [ ! -f "$HOME/.gitconfig" ] && [ ! -f "${XDG_CONFIG_HOME:-$HOME/.config}/git/config" ]; then
+                touch "${XDG_CONFIG_DIR:-$HOME/.config}/git/config"
+            fi
+
+            if ! git config --global --get user.name 1>/dev/null ; then
+                git config --global user.name "Greg Anders"
+            fi
+
+            if ! git config --global --get user.email 1>/dev/null ; then
+                read -r -p "Git email address [greg@gpanders.com]: " ans
+                git config --global user.email "${ans:-greg@gpanders.com}"
+            fi
+
+            # This repository should always use my personal email
+            git config user.email greg@gpanders.com
+
+            git config --global commit.verbose true
+            git config --global rebase.autoSquash true
+            git config --global alias.a add
+            git config --global alias.b branch
+            git config --global alias.cm commit
+            git config --global alias.co checkout
+            git config --global alias.d diff
+            git config --global alias.f fetch
+            git config --global alias.l log
+            git config --global alias.m merge
+            git config --global alias.p push
+            git config --global alias.re reset
+            git config --global alias.st status
+            git config --global alias.snapshot "!git stash && git stash apply -q"
+            git config --global alias.t tag
+
+            # Alias for just the dotfiles repo
+            git config alias.update "!git fetch origin master:master && git rebase --autostash master"
+
+            # Set sendmail settings for git
+            git config --global sendemail.smtpEncryption tls
+            git config --global sendemail.smtpServer mail.gandi.net
+            git config --global sendemail.smtpUser greg@gpanders.com
+            git config --global sendemail.smtpServerPort 587
+            ;;
+        zsh)
+            if ! hash antibody 2>/dev/null; then
+                if [ "$(uname)" = Darwin ]; then
+                    brew install getantibody/tap/antibody
+                else
+                    mkdir -p "$HOME"/.local
+                    curl -sfL git.io/antibody | sh -s - -b "$HOME"/.local/bin
+                fi
+            fi
+            ;;
+    esac
 done
 
 echo " "
