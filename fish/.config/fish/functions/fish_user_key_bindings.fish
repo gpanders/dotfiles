cdc272ebf305d117ce6640eed897ac74c3182a40
Gregory Anders <greg@gpanders.com>
2019-10-01 21:15:57 -0600

    Move fish bindings into fish_user_key_bindings function

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,13 @@
 function fish_user_key_bindings
     bind ! bind_bang
     bind -M insert ! bind_bang
+
     bind '$' bind_dollar
     bind -M insert '$' bind_dollar
+
+    bind \ew __fish_whatis_current_token
+    bind -M insert __fish_whatis_current_token
+
+    bind \es __fish_prepend_sudo
+    bind -M insert \es __fish_prepend_sudo
 end
