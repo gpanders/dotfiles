1a1290cbd0d3a124c4f8461c4e42717d10a3aadf
Gregory Anders <greg@gpanders.com>
2019-10-03 16:20:14 -0400

    Set BUKU_COLORS in buku function

--- fish/.config/fish/functions/buku.fish
+++ fish/.config/fish/functions/buku.fish
@@ -1,4 +1,5 @@
 function buku --description "Alias for buku --suggest. If no argument given, fuzzy find all bookmarks"
+    set -q BUKU_COLORS; or set -Ux BUKU_COLORS "gCdxe"
     if test (count $argv) -eq 0; and command -sq fzy
         set -l website (buku --print --format 5 | column -ts'$'\t | fzy)
 
