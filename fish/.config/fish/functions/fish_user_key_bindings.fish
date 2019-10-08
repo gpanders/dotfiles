803cf3149d3746cadd7248adf7cd1a1800dc3257
Gregory Anders <greg@gpanders.com>
2019-10-08 15:53:21 -0600

    Bind <M-f> and <M-b> to nextd/prevd-or-forward/backward-word
    This is the same thing that Alt-Left and Alt-Right are bound to

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,12 @@ function fish_user_key_bindings
     bind -M insert ! __history_previous_command
     bind -M insert '$' __history_previous_command_arguments
 
+    bind \ef nextd-or-forward-word
+    bind -M insert \ef nextd-or-forward-word
+
+    bind \eb prevd-or-backward-word
+    bind -M insert \eb prevd-or-backward-word
+
     bind \ew __fish_whatis_current_token
     bind -M insert __fish_whatis_current_token
 
