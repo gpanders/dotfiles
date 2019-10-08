51c79745ec53a3b49360dbd7984c3d0220b1d68a
Gregory Anders <greg@gpanders.com>
2019-10-08 15:51:48 -0600

    Cleanup __fzy functions

--- fish/.config/fish/functions/__fzy.fish
+++ fish/.config/fish/functions/__fzy.fish
@@ -1,25 +1,20 @@
 function __fzy_find --argument-names cmd
-    set -l cmd (string replace "%" (commandline -t) $cmd)
-    set -l selection (eval $cmd | fzy)
-    if test -n "$selection"
-        # Compensate for multiline prompts by moving the cursor
-        # \033[<N>A is the control character to move the cursor up N lines
-        printf \033\["%d"A (math (count (fish_prompt)) - 1)
-        commandline -it -- (string escape $selection)
-    end
+    set -l tok (commandline -t)
+    eval $cmd | fzy --query=$tok | read -l selection
+    and commandline -t -- (string escape $selection)
     commandline -f repaint
 end
 
 function __fzy_files
     if not set -q FZY_FIND_FILE_COMMAND
         if command -sq fd
-            set FZY_FIND_FILE_COMMAND "fd --type f --hidden --exclude '.git' --full-path %"
+            set FZY_FIND_FILE_COMMAND "fd --type f"
         else if command -sq rg
-            set FZY_FIND_FILE_COMMAND "rg --files --hidden --glob '!.git' %"
+            set FZY_FIND_FILE_COMMAND "rg --files"
         else if command -sq ag
-            set FZY_FIND_FILE_COMMAND "ag -g '' %"
+            set FZY_FIND_FILE_COMMAND "ag -g ''"
         else
-            set FZY_FIND_FILE_COMMAND "find % -type f 2>/dev/null | sed 's:^\.git/::'"
+            set FZY_FIND_FILE_COMMAND "find . -type f -not -path '*/.*' 2>/dev/null"
         end
     end
     __fzy_find $FZY_FIND_FILE_COMMAND
@@ -28,9 +23,9 @@ end
 function __fzy_dir
     if not set -q FZY_FIND_DIR_COMMAND
         if command -sq fd
-            set FZY_FIND_DIR_COMMAND "fd --type d --hidden --exclude '.git' --full-path %"
+            set FZY_FIND_DIR_COMMAND "fd --type d"
         else
-            set FZY_FIND_DIR_COMMAND "find % -type d 2>/dev/null | sed 's:^\.git/::'"
+            set FZY_FIND_DIR_COMMAND "find . -type d -not -path '*/.*' 2>/dev/null"
         end
     end
     __fzy_find $FZY_FIND_DIR_COMMAND
