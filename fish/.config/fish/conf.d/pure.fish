a47e6b3f61e20c56c348131907b6b0e0d8150bfe
Greg Anders <greg@gpanders.com>
2019-10-09 08:39:12 -0600

    Fish changes
    - Remove xdg.fish
    - Add pure.fish
    - Add fishtape and gpanders/pure fork to fishfile
    - Only run fzf.fish if fzf installed

---
+++ fish/.config/fish/conf.d/pure.fish
@@ -0,0 +1,1 @@
+set pure_show_jobs true
--- fish/.config/fish/conf.d/xdg.fish
+++
@@ -1,6 +0,0 @@
-set -q XDG_CONFIG_HOME
-or set -Ux XDG_CONFIG_HOME ~/.config
-set -q XDG_DATA_HOME
-or set -Ux XDG_DATA_HOME ~/.local/share
-set -q XDG_CACHE_HOME
-or set -Ux XDG_CACHE_HOME ~/.local/cache
