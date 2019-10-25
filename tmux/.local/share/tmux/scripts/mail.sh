20b475a899127b734b800fa651f79f7b11d18a16
Greg Anders <greg@gpanders.com>
2019-10-24 18:13:53 -0600

    Add tmux script to show number of unread mail

---
+++ tmux/.local/share/tmux/scripts/mail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mail="$HOME/.mail"
+count=$(find "$mail" -mindepth 4 -maxdepth 4 -path '*/Inbox/new/*' | wc -l)
+
+if [[ -n "$count" && "$count" -gt 0 ]]; then
+  echo "#[fg=red]${count}#[#{E:status-right-style}] unread - "
+fi
