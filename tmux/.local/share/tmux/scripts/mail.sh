34952c34c6a530a9c47df93f99e236d1a36f7e15
Greg Anders <greg@gpanders.com>
2019-11-05 07:52:45 -0700

    Remove extra whitespace from tmux new mail indicator

--- tmux/.local/share/tmux/scripts/mail.sh
+++ tmux/.local/share/tmux/scripts/mail.sh
@@ -4,5 +4,5 @@ mail="$HOME/.mail"
 count=$(find "$mail" -mindepth 4 -maxdepth 4 -path '*/Inbox/new/*' | wc -l)
 
 if [[ -n "$count" && "$count" -gt 0 ]]; then
-  echo "#[fg=red]${count}#[#{E:status-right-style}] unread - "
+  echo "#[fg=red]${count// /}#[#{E:status-right-style}] unread - "
 fi
