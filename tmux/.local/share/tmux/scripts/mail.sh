f02005443fb5700036d81a82f867d2b1940748b0
Greg Anders <greg@gpanders.com>
2019-11-19 08:21:15 -0700

    Remove unread mail indicator from tmux status line

--- tmux/.local/share/tmux/scripts/mail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mail="$HOME/.mail"
-count=$(find "$mail" -mindepth 4 -maxdepth 4 -path '*/Inbox/new/*' | wc -l)
-
-if [[ -n "$count" && "$count" -gt 0 ]]; then
-  echo "#[fg=red]${count// /}#[#{E:status-right-style}] unread - "
-fi
