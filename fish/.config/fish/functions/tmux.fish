0d1048685f0ff31c55c9cd378519c0d0121e2c5e
Gregory Anders <greg@gpanders.com>
2019-12-04 16:51:12 -0700

    Add todo.sh fish alias

--- fish/.config/fish/functions/tmux.fish
+++ fish/.config/fish/functions/tmux.fish
@@ -1,4 +1,3 @@
 function tmux -d "Alias for tmux to open config file in custom location"
     command tmux -f $HOME/.config/tmux/tmux.conf $argv
-
 end
---
+++ fish/.config/fish/functions/todo.sh.fish
@@ -0,0 +1,4 @@
+function todo.sh -d "Alias for todo.sh with custom config file location"
+    command todo.sh -d ~/.config/todo/config $argv
+end
+
