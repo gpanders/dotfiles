6a02efc7c5a602f37b26ae2b64c30371ad5df0ee
Greg Anders <greg@gpanders.com>
2019-12-13 16:03:44 -0700

    Update 'note' command and completions

--- note/.config/fish/completions/note.fish
+++ note/.config/fish/completions/note.fish
@@ -1,10 +1,21 @@
 function __fish_complete_notes
-    set -q NOTES_DIR; or set -l NOTES_DIR $HOME/Sync/Notes
+    if not set -q NOTES_DIR
+        sed -E 's/^([^[:space:]]+)=(.*)/set \1 "\2"/' < ~/.config/note/config | source
+    end
     printf "%s\n" (command ls $NOTES_DIR | sed 's/\.[^.]*$//')
+end
+
+function __fish_complete_note_tags
+    if not set -q NOTES_DIR
+        sed -E 's/^([^[:space:]]+)=(.*)/set \1 "\2"/' < ~/.config/note/config | source
+    end
+
+    awk 'tolower($1) ~ /(keywords|tags)/ {$1=""; print $0}' $NOTES_DIR/* | tr ',' '\n' | uniq | awk '{$1=$1;print}'
 end
 
 complete -k -x -c note -n '__fish_seen_subcommand_from s sh sho show' -a '(__fish_complete_notes)'
 complete -k -x -c note -n '__fish_seen_subcommand_from o op ope open' -a '(__fish_complete_notes)'
+complete -k -x -c note -n '__fish_seen_subcommand_from t ta tag tags' -a '(__fish_complete_note_tags)'
 complete -k -x -c note -n '__fish_seen_subcommand_from l li lis list'
 complete -k -x -c note -n '__fish_use_subcommand' -a 'new' -d 'Create a note'
 complete -k -x -c note -n '__fish_use_subcommand' -a 'show' -d 'Display a note'
