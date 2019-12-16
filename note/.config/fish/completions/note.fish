344284e0a588f8c7636a609e6b4dadf97f94cd5e
Gregory Anders <greg@gpanders.com>
2019-12-15 18:16:44 -0700

    Add 'preview' subcommand to 'note'

--- note/.config/fish/completions/note.fish
+++ note/.config/fish/completions/note.fish
@@ -10,11 +10,12 @@ function __fish_complete_note_tags
         sed -E 's/^([^[:space:]]+)=(.*)/set \1 "\2"/' < ~/.config/note/config | source
     end
 
-    awk 'tolower($1) ~ /(keywords|tags)/ {$1=""; print $0}' $NOTES_DIR/* | tr ',' '\n' | uniq | awk '{$1=$1;print}'
+    awk 'tolower($1) ~ /(keywords|tags)/ {$1=""; print $0}' $NOTES_DIR/*.txt | tr ',' '\n' | sort -u | awk '{$1=$1;print}'
 end
 
 complete -k -x -c note -n '__fish_seen_subcommand_from s sh sho show' -a '(__fish_complete_notes)'
 complete -k -x -c note -n '__fish_seen_subcommand_from o op ope open' -a '(__fish_complete_notes)'
+complete -k -x -c note -n '__fish_seen_subcommand_from p pr pre prev previ previe preview' -a '(__fish_complete_notes)'
 complete -k -x -c note -n '__fish_seen_subcommand_from t ta tag tags' -a '(__fish_complete_note_tags)'
 complete -k -x -c note -n '__fish_seen_subcommand_from l li lis list'
 complete -k -x -c note -n '__fish_use_subcommand' -a 'new' -d 'Create a note'
@@ -23,3 +24,4 @@ complete -k -x -c note -n '__fish_use_subcommand' -a 'open' -d 'Open a note in $
 complete -k -x -c note -n '__fish_use_subcommand' -a 'list' -d 'List notes'
 complete -k -x -c note -n '__fish_use_subcommand' -a 'grep' -d 'Search notes'
 complete -k -x -c note -n '__fish_use_subcommand' -a 'tag' -d 'List notes with given tag'
+complete -k -x -c note -n '__fish_use_subcommand' -a 'preview' -d 'Preview a note'
