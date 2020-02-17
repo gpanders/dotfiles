33599e7826a21d23a3ce264286eafb6df70723cd
Gregory Anders <greg@gpanders.com>
2020-02-17 16:04:53 -0700

    Fix __fzy_history command
    For some reason the old version would cause the whole shell to hang.
    This way is probably better anyway as it avoids the use of eval.

--- .config/fish/functions/__fzy_history.fish
+++ .config/fish/functions/__fzy_history.fish
@@ -1,6 +1,8 @@
 function __fzy_history --description "Show command history with fzy"
     history merge
-    eval (string trim "history "(commandline)) | fzy | read -l selection
+    set -l cmd (commandline)
+    test -n $cmd; or set cmd search
+    history $cmd | fzy | read -l selection
     and commandline -- $selection
     commandline -f repaint
 end
