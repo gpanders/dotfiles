5e60b61fd5d970048aaf5e1dc2cd4cafb8bbdd6c
Greg Anders <greg@gpanders.com>
2020-02-26 07:53:14 -0700

    Remove included fish functions
    These are now included by default in fish 3.1

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,6 @@
 function fish_user_key_bindings
     bind \ef nextd-or-forward-word
     bind \eb prevd-or-backward-word
-    bind \ew __fish_whatis_current_token
-    bind \es __fish_prepend_sudo
     bind \ct __fzy
     bind \cr __fzy_history
     bind \ec __fzy_cd
