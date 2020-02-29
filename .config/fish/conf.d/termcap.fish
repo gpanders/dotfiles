d269f169e7518c2d51025a2f8af26f02437f2d1f
Gregory Anders <greg@gpanders.com>
2020-02-29 10:16:56 -0700

    Set LESS_TERMCAP env vars in fish

---
+++ .config/fish/conf.d/termcap.fish
@@ -0,0 +1,7 @@
+set -gx LESS_TERMCAP_mb (printf "\e[1;31m")
+set -gx LESS_TERMCAP_md (printf "\e[1;31m")
+set -gx LESS_TERMCAP_me (printf "\e[0m")
+set -gx LESS_TERMCAP_se (printf "\e[0m")
+set -gx LESS_TERMCAP_so (printf "\e[1;44;33m")
+set -gx LESS_TERMCAP_ue (printf "\e[0m")
+set -gx LESS_TERMCAP_us (printf "\e[1;32m")
