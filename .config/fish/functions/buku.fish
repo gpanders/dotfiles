a75684a7e21a00a449206456b06b5ca7c1b54520
Gregory Anders <greg@gpanders.com>
2020-04-14 07:57:56 -0600

    Update fish buku function
    Improve fzf selection by removing the --no-sort flag and hiding the
    index of each result.

--- .config/fish/functions/buku.fish
+++ .config/fish/functions/buku.fish
@@ -1,7 +1,7 @@
 function buku --description "Alias for buku --suggest. If no argument given, fuzzy find all bookmarks"
     set -q BUKU_COLORS; or set -Ux BUKU_COLORS "gCdxe"
     if test (count $argv) -eq 0; and command -sq fzf
-        set -l website (buku --print --format 5 | column -ts'$'\t | fzf -m --no-sort -n 2,3)
+        set -l website (buku --print --format 5 | column -ts'$'\t | fzf -m --with-nth 2..)
 
         for i in $website
             set -l index (echo "$i" | awk '{print $1}')
