07483338a296b146467c2411bb453f8050276404
Gregory Anders <greg@gpanders.com>
2020-04-28 12:39:24 -0600

    Move fisher initialization to config.fish
    This ensures that fisher plugins run *after* files in the conf.d
    directory, allowing plugin customization.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,23 @@
 set -gx EDITOR vi
 set -gx VISUAL vi
 set -gx PAGER less -iMRS
+
+set -q XDG_CONFIG_HOME; or set -U $HOME/.config
+set -q XDG_DATA_HOME; or set -U $HOME/.local/share
+
+# Bootstrap fisher
+# This needs to be in config.fish instead of a separate file in conf.d so that
+# it is guaranteed to run after all scripts in the conf.d directory
+set -g fisher_path $XDG_CONFIG_HOME/fish/fisher
+
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+if not functions -q fisher
+    curl https://git.io/fisher --create-dirs -sLo $fisher_path/functions/fisher.fish
+    fish -c fisher
+end
+
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2>/dev/null
+end
