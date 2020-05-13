e0aca0297445565d2e4dfcabd325c0cd750b32e3
Gregory Anders <greg@gpanders.com>
2020-05-13 11:20:38 -0600

    Remove env vars from config.fish
    It's easier to specify these as universal vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,3 @@
-set -gx EDITOR vi
-set -gx VISUAL vi
-set -gx PAGER less -iMRS
-
 set -q XDG_CONFIG_HOME; or set -Ux XDG_CONFIG_HOME $HOME/.config
 set -q XDG_DATA_HOME; or set -Ux XDG_DATA_HOME $HOME/.local/share
 
