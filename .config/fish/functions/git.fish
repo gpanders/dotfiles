0dd4808bb6c6ae2cce9267743a957005340ab65e
Gregory Anders <greg@gpanders.com>
2020-05-22 13:31:36 -0600

    Use git wrapper script instead of a shell function
    This makes fish completions work when in the home directory. This also
    means that other tools that use git will also respect the dotfiles repo
    in the home directory.

--- .config/fish/functions/git.fish
+++
@@ -1,8 +0,0 @@
-function git --description "Wrapper around git to manage dotfiles when in home directory"
-    if test $PWD = $HOME
-        command git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
-    else
-        command git $argv
-    end
-end
-
