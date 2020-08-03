1e9f8dd001f12b57c7ff1b45adb7c59541b752c6
Gregory Anders <greg@gpanders.com>
2020-08-03 10:26:10 -0600

    fish: remove fuzzy finder from buku alias
    With no arguments, open the buku prompt instead of piping into fzf.

--- .config/fish/functions/buku.fish
+++ .config/fish/functions/buku.fish
@@ -1,13 +1,8 @@
-function buku --description "Alias for buku --suggest. If no argument given, fuzzy find all bookmarks"
+function buku
     set -q BUKU_COLORS; or set -Ux BUKU_COLORS "gCdxe"
-    if test (count $argv) -eq 0; and command -sq fzf
-        set -l website (buku --print --format 5 | column -ts'$'\t | fzf -m --with-nth 2..)
 
-        for i in $website
-            set -l index (echo "$i" | awk '{print $1}')
-            buku --print "$index"
-            buku --open "$index"
-        end
+    if test (count $argv) -eq 0
+        command buku
     else
         command buku --suggest $argv
     end
