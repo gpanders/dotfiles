9bbbc401b8ac707f1aeff0b1dc5d55a40a2aafe1
Gregory Anders <greg@gpanders.com>
2020-11-11 12:14:10 -0700

    fish: make weechat use a virtual env if one exists

--- .config/fish/functions/weechat.fish
+++ .config/fish/functions/weechat.fish
@@ -1,3 +1,8 @@
-function weechat -d "Alias for weechat with custom config location"
-    command weechat -d $HOME/.config/weechat $argv
+function weechat -d "Alias for weechat with custom config location and Python virtual env"
+    set -l venv
+    if test -n "$venv_dir"; and test -d $venv_dir/weechat
+        set venv $venv_dir/weechat/lib/python*/site-packages
+    end
+
+    PYTHONPATH=$venv command weechat -d $HOME/.config/weechat $argv
 end
