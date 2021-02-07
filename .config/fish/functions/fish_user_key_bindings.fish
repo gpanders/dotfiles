a63cc54ab4e601df9e042b50b6630a0cc8cacaf2
Gregory Anders <greg@gpanders.com>
2021-02-07 09:10:00 -0700

    Switch to fzf again

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,7 @@ function fish_user_key_bindings
     bind \ef nextd-or-forward-word
     bind \eb prevd-or-backward-word
 
-    bind \ct '__fuzzy_find fzy'
-    bind \cr '__fuzzy_history fzy'
-    bind \ec '__fuzzy_cd fzy'
+    bind \ct '__fuzzy_find fzf'
+    bind \cr '__fuzzy_history fzf'
+    bind \ec '__fuzzy_cd fzf'
 end
