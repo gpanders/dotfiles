74a40109d4e894247c90b612a0a93866f2c7e8bc
Gregory Anders <greg@gpanders.com>
2021-04-15 13:56:40 -0600

    tmux: improve backwards compatibility

--- .local/share/tmux/scripts/fzf-url.sh
+++ .local/share/tmux/scripts/fzf-url.sh
@@ -1,31 +1,40 @@
-#!/bin/sh -f
+#!/bin/sh
 
-urls=$(tmux capture-pane -J -p | grep -oE '(?:https?|file)://[-a-zA-Z0-9@:%_+.~#?&/=]+[-a-zA-Z0-9@%_+.~#?&/=]+')
+if [ -z "$DISPLAY" ]; then
+	tmux display-message 'DISPLAY is not set'
+	exit
+fi
+
+urls=$(tmux capture-pane -J -p | grep -oE '(https?|file)://[-a-zA-Z0-9@:%_+.~#?&/=]+[-a-zA-Z0-9@%_+.~#?&/=]+')
 
 if [ -z "$urls" ]; then
 	tmux display-message 'No URLs found'
 	exit
 fi
 
-header='Press CTRL-Y to copy URL to clipboard'
-selected=$(printf '%s\n' "$urls" | fzf-tmux -p -m --expect ctrl-y --header "$header")
+header='Press Enter to copy URL to clipboard or Ctrl-O to open in browser'
+selected=$(printf '%s\n' "$urls" | fzf-tmux "$@" -m --expect ctrl-o --header "$header")
+
+[ -n "$selected" ] || exit 0
 
 # shellcheck disable=2086
 set -- $selected
 
-cmd='xargs -n 1 open'
-if [ "$1" = "ctrl-y" ]; then
-	if command -v pbcopy >/dev/null; then
-		cmd=pbcopy
-	elif command -v xsel >/dev/null; then
-		cmd='xsel --clipboard --input'
-	elif command -v xclip >/dev/null; then
-		cmd='xclip -selection clipboard'
-	else
-		tmux display-message 'No command to control clipboard with'
-		exit
-	fi
+if [ "$1" = "ctrl-o" ]; then
+	shift
+	printf '%s\n' "$@" | xargs -n 1 open
+	exit
 fi
 
-shift
+if command -v pbcopy >/dev/null; then
+	cmd=pbcopy
+elif command -v xsel >/dev/null; then
+	cmd='xsel --clipboard --input'
+elif command -v xclip >/dev/null; then
+	cmd='xclip -selection clipboard'
+else
+	tmux display-message 'No command to control clipboard with'
+	exit
+fi
+
 printf '%s\n' "$@" | $cmd
