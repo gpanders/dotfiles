9f8d69c80ffb44b93eee47ef58c4b41bedd3ef00
Gregory Anders <greg@gpanders.com>
2021-04-22 12:29:02 -0600

    weechat: remove python venv

--- .config/fish/functions/weechat.fish
+++ .config/fish/functions/weechat.fish
@@ -1,8 +1,3 @@
-function weechat -d "Alias for weechat with custom config location and Python virtual env"
-    set -l venv
-    if test -n "$venv_dir"; and test -d $venv_dir/weechat
-        set venv $venv_dir/weechat/lib/python*/site-packages
-    end
-
-    PYTHONPATH=$venv command weechat -d $HOME/.config/weechat $argv
+function weechat -d "Alias for weechat with custom config location"
+    command weechat -d $HOME/.config/weechat $argv
 end
