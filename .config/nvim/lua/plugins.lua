20a8fb531bf634d4ec230a75a0c2ff9b797c75ff
Gregory Anders <greg@gpanders.com>
2021-06-10 18:57:46 -0600

    nvim: put packer in start directory
    packer doesn't contain any startup code so there is no penalty for
    keeping it under start, but it does save us from having to explicitly
    run :packadd

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,14 +1,12 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
-    use { 'wbthomason/packer.nvim', opt = true }
+    use 'wbthomason/packer.nvim'
 
     use 'gpanders/vim-medieval'
 
