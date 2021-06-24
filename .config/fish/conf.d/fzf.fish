3e104090252628a99bd1d658c7e1a7acb0b87d33
Gregory Anders <greg@gpanders.com>
2021-06-24 06:57:36 -0600

    fish: fzf: show hidden files

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,17 +1,17 @@
 status is-interactive; or exit
 
 if command -sq fd
-    set -g FZF_CTRL_T_COMMAND fd --type f --follow --no-ignore-vcs
-    set -g FZF_ALT_C_COMMAND fd -t d --no-ignore-vcs
+    set -g FZF_CTRL_T_COMMAND fd --type f --follow
+    set -g FZF_ALT_C_COMMAND fd -t d --hidden
 else if command -sq fdfind
-    set -g FZF_CTRL_T_COMMAND fdfind --type f --follow --no-ignore-vcs
-    set -g FZF_ALT_C_COMMAND fdfind -t d --no-ignore-vcs
+    set -g FZF_CTRL_T_COMMAND fdfind --type f --follow --hidden
+    set -g FZF_ALT_C_COMMAND fdfind -t d --hidden
 else if command -sq rg
-    set -g FZF_CTRL_T_COMMAND rg --files --no-ignore-vcs
-    set -g FZF_ALT_C_COMMAND find -L . -mindepth 1 -type d -print | sed 's|^\./||'
+    set -g FZF_CTRL_T_COMMAND rg --files --hidden
+    set -g FZF_ALT_C_COMMAND find -L . -mindepth 1 -type d -print
 else if command -sq ag
     set -g FZF_CTRL_T_COMMAND ag -g ''
-    set -g FZF_ALT_C_COMMAND find -L . -mindepth 1 -type d -print | sed 's|^\./||'
+    set -g FZF_ALT_C_COMMAND find -L . -mindepth 1 -type d -print
 end
 
 set -gx FZF_DEFAULT_COMMAND "$FZF_CTRL_T_COMMAND"
