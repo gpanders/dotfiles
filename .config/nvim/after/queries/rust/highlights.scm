f57de8ee3a43dfa996cd7da124c76d348e2a942f
Gregory Anders <greg@gpanders.com>
2021-07-08 10:02:03 -0600

    nvim: treesitter: remove some rust highlights

--- .config/nvim/after/queries/rust/highlights.scm
+++ .config/nvim/after/queries/rust/highlights.scm
@@ -2,6 +2,11 @@
 (use_list (identifier) @none (#match? @none "^[A-Z]"))
 (use_as_clause alias: (identifier) @none (#match? @none "^[A-Z]"))
 
+(self) @none
+
+(struct_item name: (type_identifier) @none)
+(impl_item type: (type_identifier) @none)
+
 ; Don't highlight types in scoped identifiers
 ((scoped_identifier
   path: (identifier) @none)
@@ -9,3 +14,10 @@
 ((scoped_identifier
     name: (identifier) @none)
  (#match? @none "^[A-Z]"))
+
+; Highlight macros as @macro instead of @function.macro
+(macro_invocation
+  macro: (identifier) @macro)
+(macro_invocation
+  macro: (scoped_identifier
+           (identifier) @macro .))
