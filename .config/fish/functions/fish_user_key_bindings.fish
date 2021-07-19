e3c7fdf653393c29fbb6b0380c7fa4d531a3718a
Gregory Anders <greg@gpanders.com>
2021-07-19 08:34:51 -0600

    fish: prefer doas if installed

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,6 @@ function fish_user_key_bindings
     bind \eb prevd-or-backward-word
 
     functions -q fzf_key_bindings; and fzf_key_bindings
+
+    command -sq doas; and bind \es 'fish_commandline_prepend doas'
 end
