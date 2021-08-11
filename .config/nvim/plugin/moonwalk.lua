7f1d04a85f47665a614e2d47b81a6bf93e42894c
Gregory Anders <greg@gpanders.com>
2021-08-11 10:03:11 -0600

    nvim: fix bug in keymap macro
    noremap is always set to true, regardless of what is passed in the opts
    table. Only set noremap to true if the key does not already exist in
    opts.

--- .config/nvim/plugin/moonwalk.lua
+++ .config/nvim/plugin/moonwalk.lua
@@ -31,6 +31,5 @@ local fennel = setmetatable({}, {
 
 moonwalk.add_loader("fnl", function(src)
     fennel["macro-path"] = fennel["macro-path"] .. ";" .. vim.fn.stdpath("config") .. "/fnl/?.fnl"
-    src = "(require-macros :macros)" .. src
-    return fennel.compileString(src)
+    return fennel.compileString("(require-macros :macros)" .. src)
 end)
