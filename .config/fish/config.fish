c32ac58692a2e247ec7ce30160eb0fbc8d1a3ddc
Gregory Anders <greg@gpanders.com>
2021-08-17 19:08:16 -0600

    fish: use nvim as MANPAGER

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
-set -q XDG_CONFIG_HOME; or set -Ux XDG_CONFIG_HOME $HOME/.config
-set -q XDG_DATA_HOME; or set -Ux XDG_DATA_HOME $HOME/.local/share
-set -q XDG_CACHE_HOME; or set -Ux XDG_CACHE_HOME $HOME/.cache
+set -qx XDG_CONFIG_HOME; or set -Ux XDG_CONFIG_HOME $HOME/.config
+set -qx XDG_DATA_HOME; or set -Ux XDG_DATA_HOME $HOME/.local/share
+set -qx XDG_CACHE_HOME; or set -Ux XDG_CACHE_HOME $HOME/.cache
+
+if command -sq nvim
+    set -qx EDITOR; or set -Ux EDITOR nvim
+    set -qx VISUAL; or set -Ux VISUAL nvim
+    set -qx MANPAGER; or set -Ux MANPAGER 'nvim +Man!'
+end
