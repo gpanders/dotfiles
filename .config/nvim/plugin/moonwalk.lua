1e351acaebe6b0cd9501ae60b52d482c55348074
Gregory Anders <greg@gpanders.com>
2022-01-17 13:45:36 -0700

    nvim: add bang variant to :Moonwalk to clean existing files

--- .config/nvim/plugin/moonwalk.lua
+++ .config/nvim/plugin/moonwalk.lua
@@ -5,27 +5,42 @@ augroup fennel
   autocmd SourceCmd *.fnl lua dofile(require("moonwalk").compile(vim.fn.expand("<afile>:p")))
 augroup END]], vim.fn.stdpath("config")))
 
-vim.api.nvim_add_user_command("Moonwalk", function()
-    local compile = require("moonwalk").compile
-    for _, path in ipairs({vim.fn.stdpath("config"), vim.fn.stdpath("config") .. "/after"}) do
-        for _, subpath in ipairs({"plugin", "indent", "ftplugin", "fnl", "colors"}) do
-            for _, v in ipairs(vim.fn.globpath(path, subpath .. "/*.fnl", false, true)) do
-                compile(v)
+local function walk(dir, ext, f)
+    local subdirs = {"plugin", "indent", "ftplugin", "colors"}
+    table.insert(subdirs, ext)
+    for _, path in ipairs({dir, dir .. "/after"}) do
+        for _, subpath in ipairs(subdirs) do
+            for _, v in ipairs(vim.fn.globpath(path, subpath .. "/*." .. ext, false, true)) do
+                f(v)
             end
-            for _, v in ipairs(vim.fn.globpath(path, subpath .. "/*/*.fnl", false, true)) do
-                compile(v)
+            for _, v in ipairs(vim.fn.globpath(path, subpath .. "/*/*." .. ext, false, true)) do
+                f(v)
             end
         end
     end
+end
 
+vim.api.nvim_add_user_command("Moonwalk", function(opts)
+    if opts.bang then
+        -- Clean existing files
+        walk(vim.fn.stdpath("data") .. "/site", "lua", function(path)
+            if vim.fn.fnamemodify(path, ":t") == "fennel.lua" then
+                return
+            end
+            os.remove(path)
+        end)
+    end
+
+    walk(vim.fn.stdpath("config"), "fnl", require("moonwalk").compile)
     local f = io.open(vim.fn.stdpath("cache") .. "/.compiled", "w")
     f:write("")
     f:close()
 end, {
+    bang = true,
     desc = "Compile all Fennel runtime files to Lua",
 })
 
 -- Using a filesystem marker to do this seems hacky, but I don't now of a better way
 if not vim.loop.fs_stat(vim.fn.stdpath("cache") .. "/.compiled") then
-    vim.api.nvim_command("Moonwalk")
+    vim.api.nvim_command("Moonwalk!")
 end
