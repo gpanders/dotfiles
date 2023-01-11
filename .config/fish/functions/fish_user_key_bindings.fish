ef663965b3d2bfcb81663bcd8b4cbee97ca400c6
Gregory Anders <greg@gpanders.com>
2023-01-11 09:27:23 -0700

    fish: use builtin history pager if it exists

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,6 +3,9 @@ function fish_user_key_bindings
     bind \eb prevd-or-backward-word
 
     functions -q fzf_key_bindings; and fzf_key_bindings
+    if bind --preset \cr | string match -qe history-pager
+        bind --erase --user \cr
+    end
 
     command -sq doas; and bind \es 'fish_commandline_prepend doas'
 end
