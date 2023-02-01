75938e86a03d169949a15f897b2df3c1723f5269
Gregory Anders <greg@gpanders.com>
2023-02-01 08:45:22 -0700

    fish: update zoxide functions from upstream

--- .config/fish/completions/z.fish
+++ .config/fish/completions/z.fish
@@ -1,7 +1,7 @@
 # Completions for `z`.
 function __zoxide_z_complete
-    set -l tokens (commandline -op)
-    set -l curr_tokens (commandline -cop)
+    set -l tokens (commandline --current-process --tokenize)
+    set -l curr_tokens (commandline --cut-at-cursor --current-process --tokenize)
 
     if test (count $tokens) -le 2 -a (count $curr_tokens) -eq 1
         # If there are < 2 arguments, use `cd` completions.
@@ -9,8 +9,8 @@ function __zoxide_z_complete
     else if test (count $tokens) -eq (count $curr_tokens)
         # If the last argument is empty, use interactive selection.
         set -l query $tokens[2..-1]
-        set -l result (zoxide query -i -- $query)
-        and commandline -p "$tokens[1] "(string escape $result)
+        set -l result (zoxide query --exclude $PWD -i -- $query)
+        and echo z!$result
         commandline -f repaint
     end
 end
--- .config/fish/functions/z.fish
+++ .config/fish/functions/z.fish
@@ -1,12 +1,16 @@
 # Jump to a directory using only keywords.
 function z
-    set argc (count $argv)
+    set -l argc (count $argv)
+    set -l completion_regex '^'(string escape --style=regex 'z!')'(.*)$'
+
     if test $argc -eq 0
         cd $HOME
     else if test "$argv" = -
         cd -
     else if test $argc -eq 1 -a -d $argv[1]
         cd $argv[1]
+    else if set -l result (string match --groups-only --regex $completion_regex $argv[-1])
+        cd $result
     else
         set -l result (command zoxide query --exclude $PWD -- $argv)
         and cd $result
