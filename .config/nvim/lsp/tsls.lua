10f59dd49cbaf1ed38a9dbc969a080a71dd1f5f1
Gregory Anders <greg@gpanders.com>
2024-12-18 11:40:39 -0600

    nvim: use typescriptreact filetype for tsls

--- .config/nvim/lsp/tsls.lua
+++ .config/nvim/lsp/tsls.lua
@@ -1,5 +1,5 @@
 return {
-    filetype = "typescript",
+    filetype = { "typescript", "typescriptreact" },
     cmd = { "typescript-language-server", "--stdio" },
     root_dir = vim.fs.root(0, { "pnpm-lock.yaml", "tsconfig.json" }),
 }
