ae95104dcbe9bb375ba930941d99e97345b6e384
Gregory Anders <greg@gpanders.com>
2024-12-18 20:52:30 -0600

    nvim: lsp: support function for root_dir

--- .config/nvim/lsp/gopls.lua
+++ .config/nvim/lsp/gopls.lua
@@ -1,7 +1,14 @@
 return {
     filetype = { "go", "gomod", "gowork", "gosum" },
     cmd = { "gopls" },
-    root_dir = vim.fs.root(0, { "go.work", "go.mod" }),
+    root_dir = function()
+        local root = vim.fs.root(0, { "go.mod" })
+        local workspace_root = vim.fs.root(root, { "go.work" })
+        if workspace_root then
+            return workspace_root
+        end
+        return root
+    end,
     settings = {
         autoformat = true,
         gopls = {
--- .config/nvim/lsp/rust-analyzer.lua
+++ .config/nvim/lsp/rust-analyzer.lua
@@ -1,7 +1,20 @@
 return {
     filetype = "rust",
     cmd = { "rust-analyzer" },
-    root_dir = vim.fs.root(0, { "Cargo.toml" }),
+    root_dir = function()
+        local root = vim.fs.root(0, { "Cargo.toml" })
+        local out = vim.system({"cargo", "metadata", "--no-deps", "--format-version", "1"}, { cwd = root }):wait()
+        if out.code ~= 0 then
+            return root
+        end
+
+        local ok, result = pcall(vim.json.decode, out.stdout)
+        if ok and result.workspace_root then
+            return result.workspace_root
+        end
+
+        return root
+    end,
     settings = {
         autoformat = true,
         ["rust-analyzer"] = {
