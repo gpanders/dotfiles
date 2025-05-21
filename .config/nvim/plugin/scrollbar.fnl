gpanders/dotfiles

commit b4c9e35a1c3dbc1681d997c4e9543c83705dd25e

nvim: disable scrollbar in floating windows

.config/nvim/plugin/scrollbar.fnl
@@ -2,17 +2,19 @@
 (local state {})
 
 (fn on-win [_ winid bufnr topline botline]
-  (let [lines (vim.api.nvim_buf_line_count bufnr)
-        height (vim.api.nvim_win_get_height winid)]
-    (if (<= lines height)
-        (tset state winid nil)
-        (let [cells-per-line (/ height lines)
-              span (math.floor (+ 0.5 (* (- botline topline) cells-per-line)))
-              start (math.floor (+ topline (* topline cells-per-line)))
-              end (math.min lines (+ start span 1))]
-          (tset state winid {: start : end}))))
-  (vim.api.nvim__redraw {:win winid :valid false})
-  (not= nil (. state winid)))
+  (if (not= "" (. (vim.api.nvim_win_get_config winid) :relative))
+      false
+      (let [lines (vim.api.nvim_buf_line_count bufnr)
+            height (vim.api.nvim_win_get_height winid)]
+        (if (<= lines height)
+            (tset state winid nil)
+            (let [cells-per-line (/ height lines)
+                  span (math.floor (+ 0.5 (* (- botline topline) cells-per-line)))
+                  start (math.floor (+ topline (* topline cells-per-line)))
+                  end (math.min lines (+ start span 1))]
+              (tset state winid {: start : end}))))
+      (vim.api.nvim__redraw {:win winid :valid false})
+      (not= nil (. state winid))))
 
 (fn on-line [_ winid bufnr row]
   (case (. state winid)

commit 8d22dc2a77c8e2928037d3c1102e968bcefa77e0

nvim: fix scrollbar
This has been broken since b4c9e35a1c3dbc1681d997c4e9543c83705dd25e.

.config/nvim/plugin/scrollbar.fnl
@@ -12,9 +12,9 @@
                   span (math.floor (+ 0.5 (* (- botline topline) cells-per-line)))
                   start (math.floor (+ topline (* topline cells-per-line)))
                   end (math.min lines (+ start span 1))]
-              (tset state winid {: start : end}))))
-      (vim.api.nvim__redraw {:win winid :valid false})
-      (not= nil (. state winid))))
+              (tset state winid {: start : end})))
+        (vim.api.nvim__redraw {:win winid :valid false})
+        (not= nil (. state winid)))))
 
 (fn on-line [_ winid bufnr row]
   (case (. state winid)