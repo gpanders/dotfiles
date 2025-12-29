dceea2145f305b601850fbf18e6399a547bc0097
Gregory Anders <greg@gpanders.com>
2025-12-29 08:53:56 -0600

    fish: use global var for fish_key_bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 status is-interactive; or exit
 
+set -g fish_key_bindings fish_default_key_bindings
+
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CACHE_HOME $HOME/.cache
